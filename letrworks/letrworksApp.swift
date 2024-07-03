b21caf0eddaa616bcb9f01f7ea700b12e2d7a9b5
hongsungin92 <hongsungin92@gmail.com>
2024-07-04 00:29:57 +0900

    Initial Commit

---
+++ letrworks/letrworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  letrworksApp.swift
+//  letrworks
+//
+//  Created by Sungin Hong on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct letrworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
