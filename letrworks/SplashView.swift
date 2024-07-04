831035fe93eb2ce4a07dd4381b124570b2536dda
hongsungin92 <hongsungin92@gmail.com>
2024-07-05 02:03:57 +0900

    login view, splash view

---
+++ letrworks/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  letrworks
+//
+//  Created by Sungin Hong on 7/4/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
